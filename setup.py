42d0ef729e31a92e55fda1683b734cb6055d7e68
unknown <andrew.e.cumming@gmail.com>
2022-09-13 13:10:14 -0400

    Added missing package in setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
         author='Andrew Cumming',
         author_email='andrew.e.cumming@gmail.com',
         url='https://github.com/AndrewEC/py-rot-cipher',
-        packages=['rotcipher']
+        packages=['rotcipher', 'rotcipher.lib']
     )
